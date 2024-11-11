af8ace69394224c1bc3c9bb313e48a89421968aa
DivinerX <loc_yan@outlook.com>
2024-11-11 07:27:07 +0000

    auth middleware

---
+++ models/property.model.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"noticepros/utils"
+
+	"gorm.io/gorm"
+)
+
+type Property struct {
+	ID           string `gorm:"type:string;primaryKey"`
+	Name         string `gorm:"column:name"`
+	Address      string
+	City         string `gorm:"column:cty"`
+	Unit         string `gorm:"column:unit"`
+	State        string `gorm:"column:st"`
+	ZipCode      string `gorm:"column:zip"`
+	County       string `gorm:"column:cnty"`
+	NumUnitTotal uint8
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+func (property *Property) BeforeCreate(tx *gorm.DB) (err error) {
+	property.ID = utils.RandomString(10)
+	return
+}
